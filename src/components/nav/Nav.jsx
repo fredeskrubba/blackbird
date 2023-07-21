81bcc11899251a232616bbed252fd871ec267862
fredeskrubba <frederikskrubbeltrang@outlook.dk>
2023-07-21 12:00:31 +0200

    add loginmodal logic

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -5,12 +5,16 @@ import menuIcon from "../../assets/icons/menu-icon.svg"
 import searchIcon from "../../assets/icons/search-icon.svg"
 import profileIcon from "../../assets/icons/profile-icon.svg"
 import blogIcon from "../../assets/icons/blog-icon.svg"
+import cancelIcon from "../../assets/icons/cancel-icon.svg"
 import Link from 'next/link'
 import useUserStore from '@/stores/userStore'
 import { useState } from 'react'
 
 const Nav = ({home}) => {
     const [colorChange, setColorChange] = useState(false)
+    const [showLoginModal, setShowLoginModal] = useState(false)
+    const [username, setUserName] = useState("")
+    const [password, setPassword] = useState("")
     const loggedIn = useUserStore((state) => state.loggedIn)
     const changeNavbarColor = () => {
         if (window.scrollY >= 300) {
@@ -23,13 +27,13 @@ const Nav = ({home}) => {
     window.addEventListener('scroll', changeNavbarColor);
 
     return (
-        <nav className={` ${colorChange ? "bg-light shadow-md border-bronze border-b-4" : ""} ${home ? "" : "bg-light shadow-md border-bronze border-b-4" } flex justify-between px-24  fixed w-full m-0 z-10`}>
+        <nav className={` ${colorChange ? "bg-light shadow-md border-bronze border-b-4" : ""} ${home ? "" : "bg-light shadow-md border-bronze border-b-4" } flex justify-between px-24  fixed w-full m-0 z-10 py-6`}>
             <Image
                 src={menuIcon}
                 width={50}
                 height={50}
                 alt="menu-button"
-                className='hover:cursor-pointer py-6'
+                className='hover:cursor-pointer'
             />
             <div className='flex gap-8'>
             {
@@ -43,15 +47,15 @@ const Nav = ({home}) => {
                         className='hover:cursor-pointer'
                     />
                 </Link> :
-                <Link href="/logIn">
                     <Image
                         src={profileIcon}
                         width={50}
                         height={50}
                         alt="profile-button"
                         className='hover:cursor-pointer'
+                        onClick={()=>{setShowLoginModal(true)}}
                     />
-                </Link> 
+                
 
             }
             
@@ -72,18 +76,23 @@ const Nav = ({home}) => {
                     />
                 </Link>
             </div>
-            <article className='bg-trans-dark fixed right-0 left-0 py-20 px-80 h-full flex flex-col justify-between '>
+            <article className={` bg-trans-dark fixed right-0 left-0 top-0 bottom-0 py-20 px-80 h-full flex-col justify-between ${showLoginModal ? "flex" : "hidden"}`}>
                 <div className='flex flex-col bg-bronze px-10 py-10 items-center rounded-lg justify-between'>
                     <section className='flex flex-col gap-8 w-full'>
-                        <article className='flex flex-col gap-4'>
-                            <h2 className='text-light font-caveat text-5xl font-bold'>Log in</h2>
-                            <div className='p-0.5 bg-light w-28 -rotate-3 z-0'></div>
-                        </article>
-                        <input type="text" placeholder='Brugernavn' className='rounded-full px-5 py-3 text-bronze text-2xl placeholder:text-bronze placeholder:font-semibold focus:outline-none'/>
-                        <input type="text" placeholder='Password' className='rounded-full px-5 py-3 text-bronze text-2xl placeholder:text-bronze placeholder:font-semibold focus:outline-none'/>
+                        <div className='flex justify-between items-center'>
+                            <article className='flex flex-col gap-4'>
+                                <h2 className='text-light font-caveat text-5xl font-bold'>Log in</h2>
+                                <div className='p-0.5 bg-light w-28 -rotate-3 z-0'></div>
+                            </article>
+                            <Image src={cancelIcon} alt='cancel' width={40} height={40} className='hover:cursor-pointer' onClick={()=>{setShowLoginModal(false)}}/>
+                        </div>
+                        <input type="text" placeholder='Brugernavn' className='rounded-full px-5 py-3 text-bronze text-2xl placeholder:text-bronze placeholder:font-semibold focus:outline-none' onChange={(e)=>{setUserName(e.target.value)}}/>
+                        <input type="text" placeholder='Password' className='rounded-full px-5 py-3 text-bronze text-2xl placeholder:text-bronze placeholder:font-semibold focus:outline-none' onChange={(e)=>{setPassword(e.target.value)}}/>
 
                     </section>
-                    <p className='rounded-full bg-bronze border-solid border-light border-2 px-6 py-4 mt-12 text-light text-2xl w-1/3 text-center hover:cursor-pointer hover:bg-light hover:text-bronze hover:border-bronze'>Log in</p>
+                    <p className='rounded-full bg-bronze border-solid border-light border-2 px-6 py-4 mt-12 text-light text-2xl w-1/3 text-center hover:cursor-pointer hover:bg-light hover:text-bronze hover:border-bronze' onClick={()=>{
+                        alert(password)
+                    }}>Log in</p>
                 </div>
             </article>
         </nav>
