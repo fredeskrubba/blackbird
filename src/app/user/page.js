436855232a595118d6b78acc82f9709ac898b3e6
fredeskrubba <frederikskrubbeltrang@outlook.dk>
2023-08-05 13:01:49 +0200

    add user page fields

--- src/app/user/page.js
+++ src/app/user/page.js
@@ -11,33 +11,73 @@ const User = () => {
     <main>
         <Nav/>
         <article className='flex pt-40 mx-24 border-b-4 border-bronze py-4 justify-between items-center'>
-        <div className='flex'>
-          <h2 className='font-caveat text-bronze text-6xl font-semibold'>{user.userName}</h2>
-          <Image
-            src={editIcon}
-            alt='edit-icon'
-            width={30}
-            height={30}
-            className='relative top-4 left-2 hover:cursor-pointer '
-          />
-        </div>
-        <div className='flex'>
-          <Image 
-            src={user.profilePic}
-            alt="profile-picture"
-            height={50}
-            width={70}
-            className='border-4 border-bronze rounded-full'
-          />
-          <Image
+          <div className='flex'>
+            <h2 className='font-caveat text-bronze text-6xl font-semibold'>{user.userName}</h2>
+            <Image
               src={editIcon}
               alt='edit-icon'
               width={30}
               height={30}
-              className='relative right-8 top-8 hover:cursor-pointer'
+              className='relative top-4 left-2 hover:cursor-pointer '
             />
-        </div>
-          
+          </div>
+          <div className='flex'>
+            <Image 
+              src={user.profilePic}
+              alt="profile-picture"
+              height={50}
+              width={70}
+              className='border-4 border-bronze rounded-full'
+            />
+            <Image
+                src={editIcon}
+                alt='edit-icon'
+                width={30}
+                height={30}
+                className='relative right-8 top-8 hover:cursor-pointer'
+              />
+          </div>
+        </article>
+        <article className='flex flex-col gap-8 mx-24 mt-8'>
+          <div>
+            <h2 className='font-semibold italic text-xl mb-4'>Fornavn</h2>
+            <article className="flex gap-10">
+              <input type="text" placeholder={user.firstName} className='border-2 border-bronze text-bronze placeholder:text-bronze px-4 text-xl font-semibold'/>
+              <Image
+                    src={editIcon}
+                    alt='edit-icon'
+                    width={50}
+                    height={50}
+                    className='hover:cursor-pointer'
+                  />
+            </article>
+          </div>
+          <div>
+            <h2 className='font-semibold italic text-xl mb-4'>Efternavn</h2>
+            <article className="flex gap-10">
+              <input type="text" placeholder={user.lastName} className='border-2 border-bronze text-bronze placeholder:text-bronze px-4 text-xl font-semibold'/>
+              <Image
+                    src={editIcon}
+                    alt='edit-icon'
+                    width={50}
+                    height={50}
+                    className='hover:cursor-pointer'
+                  />
+            </article>
+          </div>
+          <div>
+            <h2 className='font-semibold italic text-xl mb-4'>Password</h2>
+            <article className="flex gap-10">
+              <input type="text" placeholder={user.password} className='border-2 border-bronze text-bronze placeholder:text-bronze px-4 text-xl font-semibold'/>
+              <Image
+                    src={editIcon}
+                    alt='edit-icon'
+                    width={50}
+                    height={50}
+                    className='hover:cursor-pointer'
+                  />
+            </article>
+          </div>
         </article>
     </main>
   )
