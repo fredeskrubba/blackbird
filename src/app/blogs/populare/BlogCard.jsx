967e9258a174b8e7160484d014684d899fdc3b51
fredeskrubba <frederikskrubbeltrang@outlook.dk>
2023-08-14 13:14:16 +0200

    add card rework

--- src/app/blogs/populare/BlogCard.jsx
+++ src/app/blogs/populare/BlogCard.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
 import Image from 'next/image'
 
-const BlogCard = ({img, tags, headline, description}) => {
+const BlogCard = ({img, tags, headline, description, author}) => {
   return (
-    <article className='border-bronze border-4 pb-10 w-full'>
-        <Image src={img} alt="blog" width={50} height={200} unoptimized={true} style={{width: '100%'}}/>
-        <section className='px-4 py-2'>
-          <div className='flex gap-3 my-3'>
-              {tags.map(({tag, id})=> <p key={id} className='border-2 rounded-full text-bronze font-bold px-4'>{tag}</p>)}
-          </div>
-          <h3 className='text-dark font-semibold text-3xl mb-4'>{headline}</h3>
-          <p className='text-dark font-normal text-xl'>{description}</p>
+    <article className='border-bronze border-4 w-full grid-cols-[1fr,3fr] grid'>
+        <Image src={img} alt="blog" width={50} height={100} unoptimized={true} style={{width: '100%', height: "100%", objectFit: "cover"}}/>
+        <section className='px-8 py-6 flex flex-col justify-between relative'>
+          <article className='pb-32'>
+            <h3 className='text-dark font-semibold text-5xl mb-4'>{headline}</h3>
+            <p className='text-dark font-normal text-2xl'>{description}</p>
+          </article>
+          <article className='flex items-center w-full gap-4 justify-end'>
+            <p>{`${author.firstName} ${author.lastName}`}</p>
+            <Image src={author.profilePic} width={50} height={50} alt='profile-pic'/>
+          </article>
+          <article className='flex absolute -bottom-6 gap-6'>
+            {tags.slice(0,3).map(({tag, key}) => <div key={key} className='text-bronze font-semibold border-4 border-bronze px-6 py-2 text-2xl bg-light'>{tag}</div>)}
+          </article>
         </section>
     </article>
   )
--- src/app/blogs/populare/PopulareSection.jsx
+++ src/app/blogs/populare/PopulareSection.jsx
@@ -23,14 +23,14 @@ const PopulareSection = () => {
     }, [])
 
   return (
-    <section className='pt-52 flex flex-col'>
+    <section className='pt-40 flex flex-col'>
         <article className='flex flex-col gap-4 px-24'>
             <h2 className='text-bronze font-caveat text-6xl font-bold'>Hot lige nu</h2>
             <div className='p-0.5 bg-bronze w-56 -rotate-3 z-0'></div>
         </article>
-        <div className='mt-16 px-24 grid grid-cols-2 gap-60 mb-32'>
+        <div className='mt-16 px-24 mb-32 flex flex-col gap-20'>
             {
-                hotBlogs.map(({img, tags, title, description, id}) => <BlogCard key={id} img={img} tags={tags} headline={title} description={description}/>)
+                hotBlogs.map(({img, tags, title, description, id, author}) => <BlogCard key={id} img={img} tags={tags} headline={title} description={description} author={author}/>)
             }
         </div>
         <article className='flex gap-12 my-12 bg-dark px-24 py-6'>
@@ -42,14 +42,14 @@ const PopulareSection = () => {
             <p className='text-bronze font-semibold text-3xl'>Nyeste</p>
             <Image src={arrowIcon} alt='arrow' width={25} height={25}/>
         </div>
-        <div className='mt-16 px-24 grid grid-cols-2 gap-60 mb-48'>
+        <div className='mt-16 px-24 flex flex-col gap-20 mb-48'>
             {
-                hotBlogs.map(({img, tags, title, description, id}) => <BlogCard key={id} img={img} tags={tags} headline={title} description={description}/>)
+                hotBlogs.map(({img, tags, title, description, id, author}) => <BlogCard key={id} img={img} tags={tags} headline={title} description={description} author={author}/>)
             }
         </div>
-        <footer className='bg-dark p-56 flex items-center flex-col gap-28'>
-            <h2 className='text-6xl text-center text-bronze font-bold w-1/2 '>Mangler der noget? Lav din egen blogpost!</h2>
-            <Link href='/create-blog'><p className='text-6xl font-bold bg-bronze text-dark py-12 px-36 rounded-full'>Kom I gang</p></Link>
+        <footer className='bg-dark px-56 py-24 flex items-center flex-col gap-20'>
+            <h2 className='text-5xl text-center text-bronze font-bold w-1/2 '>Mangler der noget? Lav din egen blogpost!</h2>
+            <Link href='/create-blog'><p className='text-5xl font-bold bg-bronze text-dark py-12 px-36'>Kom I gang</p></Link>
         </footer>
     </section>
   )
